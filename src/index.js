22f1d286aa9aee447c40c1d8c2e7db9c0856b7fe
nabil19 <funity111@gmail.com>
2022-10-25 23:32:30 +0100

    6-class

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import Greet from './components/Greet';
-import {Greet} from './components/Greet';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  
-    <Greet/>
+    <App/>
+    
   </React.StrictMode>
 );
 
