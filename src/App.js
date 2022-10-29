41efbe9b73e71ff26776115f3f9c4099cce8db4d
nabil19 <funity111@gmail.com>
2022-10-29 12:41:00 +0100

    11-setState

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Greet} from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
+import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
@@ -13,8 +14,9 @@ function App() {
         {/* <Welcome/>
         <Greet name="rayane" summoner="rayanexrayane"><p>je suis un gros noob</p></Greet>
         <Greet name="Nabil" summoner="nabil19"/>
-        <Welcome name="Nabil" summoner="nabil19"/>*/} 
-        <Message/>
+        <Welcome name="Nabil" summoner="nabil19"/>
+        <Message/>*/} 
+        <Counter/>
     </div>
   );
 }
